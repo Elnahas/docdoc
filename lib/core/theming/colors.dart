2664ec43171da2557f4173c3a931d3a88f364cfd
Hazem <hazemelnahas2010@gmail.com>
2024-04-25 20:38:18 +0200

    - add images and svg - add flutter svg and screen util - made a file route - add navigation - add color and test style - add login screen - add ombordin screen

---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ColorsManager {
+
+  static const Color mainBlue  = Color(0xFF247CFF);
+  static const Color gray  = Color(0xFF757575);
+  static const Color white  = Color(0xFFFFFFFF);
+
+}
