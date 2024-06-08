b2f5aa345aea71b24a127d3cc1273029141efcc7
Hazem <hazemelnahas2010@gmail.com>
2024-06-08 14:43:04 +0300

    - add shared_pref_helper - set header to update Authorization - chk user if login

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_omar_ahmed/core/di/dependency_injection.dart';
+import 'package:flutter_advanced_omar_ahmed/core/helpers/constants.dart';
+import 'package:flutter_advanced_omar_ahmed/core/helpers/extentions.dart';
+import 'package:flutter_advanced_omar_ahmed/core/helpers/shared_pref_helper.dart';
 import 'package:flutter_advanced_omar_ahmed/core/routing/app_router.dart';
 import 'package:flutter_advanced_omar_ahmed/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
-  runApp( DocApp(appRouter: AppRouter(),));
+  // To fix texts being hidden bug flutter_screenutil in release mode
+  await ScreenUtil.ensureScreenSize();
+  await checkIfLoggedInUser();
+  runApp(DocApp(
+    appRouter: AppRouter(),
+  ));
+}
+
+checkIfLoggedInUser() async {
+  String? userToken =
+      await SharedPrefHelper.getString(SharedPrefKeys.userToken);
+  if (!userToken.isNullOrEmpty()) {
+    isLoggedInUser = true;
+  } else {
+    isLoggedInUser = false;
+  }
 }
